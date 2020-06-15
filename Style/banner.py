ecb160e5f528fbb0f37f8ca7b8985b4fea0fe6ac
Chr0m0s0m3s <53451298+Chr0m0s0m3s@users.noreply.github.com>
2020-06-15 21:57:46 +0630

    Create banner.py

---
+++ Style/banner.py
@@ -0,0 +1,14 @@
+import os
+import random
+
+class colors:
+    yellow =  '\033[1;33m'
+
+def banner():
+    path ='/home/chr0m0s0m3s/Templates/Style/logos/'
+    files = os.listdir(path)
+    index = random.randrange(0, len(files))
+    f = open(f'/home/chr0m0s0m3s/Templates/Style/logos/{files[index]}' , 'r')
+    contents = f.read()
+    print(colors.yellow + contents)
+    f.close()
