506b1d6e4ae652788421e7782f3905fd44b83443
Chr0m0s0m3s <53451298+Chr0m0s0m3s@users.noreply.github.com>
2020-06-17 00:49:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ OS_bit = (round(math.log(sys.maxsize,2)+1))
 
 os.system("sudo apt-get install python3-pip")   
 os.system("pip3 install -U selenium")
+os.system("pip3 install python-dotenv")
 
 print("\n \n {} \n \n".format(OS_bit))
 
