a1233b0f9e24d178a2191c9fbdeefd3fa7b755b6
Chr0m0s0m3s <53451298+Chr0m0s0m3s@users.noreply.github.com>
2020-06-17 20:59:34 +0630

    Update banner.py

--- Style/banner.py
+++ Style/banner.py
@@ -5,10 +5,10 @@ class colors:
     yellow =  '\033[1;33m'
 
 def banner():
-    path ='/logos/'
+    path ='DeadTrap/Style/logos/'
     files = os.listdir(path)
     index = random.randrange(0, len(files))
-    f = open(f'/logos/{files[index]}' , 'r')
+    f = open(f'DeadTrap/Style/logos/{files[index]}' , 'r')
     contents = f.read()
     print(colors.yellow + contents)
     f.close()
