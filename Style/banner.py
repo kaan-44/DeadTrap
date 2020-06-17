8abbb232e9a4a7a8bfd1677443ac71ab03a2813a
Chr0m0s0m3s <53451298+Chr0m0s0m3s@users.noreply.github.com>
2020-06-17 16:38:23 +0530

    Update banner.py

--- Style/banner.py
+++ Style/banner.py
@@ -8,7 +8,7 @@ def banner():
     path ='/logos/'
     files = os.listdir(path)
     index = random.randrange(0, len(files))
-    f = open(f'/home/chr0m0s0m3s/Templates/Style/logos/{files[index]}' , 'r')
+    f = open(f'/logos/{files[index]}' , 'r')
     contents = f.read()
     print(colors.yellow + contents)
     f.close()
